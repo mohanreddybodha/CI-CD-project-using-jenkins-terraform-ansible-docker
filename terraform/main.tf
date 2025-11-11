8fb2b696158c48e83bd63593085004a2d1bea904
mohanreddybodha <bodhamohan reddy05@gmail.com>
2025-11-11 14:14:27 +0530

    modified:   terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,14 @@ resource "aws_security_group" "web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "Allow ssh traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   egress {
     description = "Allow all outbound traffic"
