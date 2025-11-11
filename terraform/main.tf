d9b7608ea622c539c5e1f254787fec68dfedf83f
mohanreddybodha <bodhamohan reddy05@gmail.com>
2025-11-11 13:26:19 +0530

    modified:   jenkinsfile,terraform/main.tf files

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,14 +9,26 @@ provider "aws" {
 }
 
 resource "aws_security_group" "web_sg" {
-  name        = "flask-ui-sg"
-  description = "Allow HTTP(80) and SSH(22)"
+  name        = "web-sg"
+  description = "Allow inbound traffic for web UI"
 
-  ingress { from_port = 80  to_port = 80  protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
-  ingress { from_port = 22  to_port = 22  protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  egress  { from_port = 0   to_port = 0   protocol = "-1"  cidr_blocks = ["0.0.0.0/0"] }
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
 
 resource "aws_instance" "web" {
   ami                    = "ami-0f58b397bc5c1f2e8" # Amazon Linux 2, ap-south-1 (verify before apply)
